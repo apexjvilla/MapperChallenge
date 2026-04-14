93dc02e6b2ae279cf3c19ad9e6faa2f193d075be
Julio Villa <jvilla@apexsystems.com>
2026-04-14 11:38:11 -0600

    Created first version of Map method

--- src/ConsoleApp/ConsoleApp/MapperUtility.cs
+++ src/ConsoleApp/ConsoleApp/MapperUtility.cs
@@ -1,10 +1,39 @@
-﻿namespace ConsoleApp
+﻿using System.Reflection;
+
+namespace ConsoleApp
 {
     public static class MapperUtility
     {
         public static TTarget Map<TSource, TTarget>(TSource source) where TTarget : new()
         {
-            return new TTarget();
+            if(source is null)
+                throw new ArgumentNullException(nameof(source));
+
+            TTarget target = new();
+
+            PropertyInfo[] sourceProperties = typeof(TSource)
+                .GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            PropertyInfo[] targetProperties = typeof(TTarget)
+                .GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            foreach (PropertyInfo targetProp in targetProperties)
+            {
+                if (!targetProp.CanWrite) continue;
+
+                PropertyInfo? sourceProp = sourceProperties.FirstOrDefault(sp => 
+                        sp.Name == targetProp.Name 
+                        && sp.PropertyType == targetProp.PropertyType
+                        && sp.CanRead);
+
+                if (sourceProp is null) continue;
+
+                var value = sourceProp.GetValue(source);
+
+                targetProp.SetValue(target, value);
+            }
+
+            return target;
         }
     }
 }
