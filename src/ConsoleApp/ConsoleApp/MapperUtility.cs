60891f0b85e39e46d0fd828363a16e482ce591dd
Julio Villa <jvilla@apexsystems.com>
2026-04-14 12:30:12 -0600

    Add concurrent dictionary to cache properties to avoid repeated reflection

--- src/ConsoleApp/ConsoleApp/MapperUtility.cs
+++ src/ConsoleApp/ConsoleApp/MapperUtility.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Collections.Concurrent;
+using System.Reflection;
 
 namespace ConsoleApp
 {
@@ -9,28 +10,42 @@ namespace ConsoleApp
             if(source is null)
                 throw new ArgumentNullException(nameof(source));
 
+            // Cache properties per type to avoid repeated reflection
+            ConcurrentDictionary<Type, PropertyInfo[]> propertiesCache = new();
+
             TTarget target = new();
 
-            PropertyInfo[] sourceProperties = typeof(TSource)
-                .GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            PropertyInfo[] sourceProperties = propertiesCache.GetOrAdd(
+                typeof(TSource),
+                t=>t.GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                );
 
-            PropertyInfo[] targetProperties = typeof(TTarget)
-                .GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            PropertyInfo[] targetProperties = propertiesCache.GetOrAdd(
+                typeof(TTarget),
+                t=>t.GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                );
 
             foreach (PropertyInfo targetProp in targetProperties)
             {
                 if (!targetProp.CanWrite) continue;
 
-                PropertyInfo? sourceProp = sourceProperties.FirstOrDefault(sp => 
-                        sp.Name == targetProp.Name 
-                        && sp.PropertyType == targetProp.PropertyType
-                        && sp.CanRead);
+                try
+                {
+                    PropertyInfo? sourceProp = sourceProperties.FirstOrDefault(sp =>
+                            sp.Name == targetProp.Name
+                            && sp.PropertyType == targetProp.PropertyType
+                            && sp.CanRead);
 
-                if (sourceProp is null) continue;
+                    if (sourceProp is null) continue;
 
-                var value = sourceProp.GetValue(source);
+                    var value = sourceProp.GetValue(source);
 
-                targetProp.SetValue(target, value);
+                    targetProp.SetValue(target, value);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error mapping {targetProp.Name}: {ex.Message}");
+                }
             }
 
             return target;
